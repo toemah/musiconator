998dc1f9b1de06a0c17261b5162fd0441c738a4a
toemah <toemah@protonmail.com>
2022-05-21 17:10:14 +0200

    Added Hive set up and sound test

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:musiconator/main.dart';
 
@@ -8,14 +9,32 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  late AudioCache player = AudioCache(prefix: 'assets/audio/');
+
+  @override
+  void initState() {
+    super.initState();
+    player.load('explosion.mp3');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    player.clearAll();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(MyApp.title),
+        title: const Text(MyApp.title),
       ),
       body: const Center(
         child: Text("Hello World"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {player.play('explosion.mp3')},
+        child: const Icon(Icons.play_arrow),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:musiconator/homepage.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:musiconator/sound.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(SoundAdapter());
+  await Hive.openBox<Sound>(MyApp.soundBox);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static String title = "Musiconator";
+  static const String title = "Musiconator";
+  static const String soundBox = "sound";
 
   @override
   Widget build(BuildContext context) {
