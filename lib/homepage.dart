d39a84b8d541805e157dccdd306d27d81f249615
toemah <toemah@protonmail.com>
2022-05-22 15:20:26 +0200

    Extracted sound logic to its own screen

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,3 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:musiconator/main.dart';
 
@@ -9,19 +8,6 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-  late AudioCache player = AudioCache(prefix: 'assets/audio/');
-
-  @override
-  void initState() {
-    super.initState();
-    player.load('explosion.mp3');
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    player.clearAll();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,10 +17,6 @@ class _HomepageState extends State<Homepage> {
       ),
       body: const Center(
         child: Text("Hello World"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => {player.play('explosion.mp3')},
-        child: const Icon(Icons.play_arrow),
       ),
     );
   }
