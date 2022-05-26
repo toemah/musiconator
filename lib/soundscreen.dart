2f3d7e823692b34cc667c2e7e388b877c8f6f7a4
pperez91100 <105656438+pperez91100@users.noreply.github.com>
2022-05-26 20:21:19 +0200

    Update soundscreen.dart
    Soundscreen design

--- lib/soundscreen.dart
+++ lib/soundscreen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:musiconator/main.dart';
 
@@ -6,7 +8,7 @@ class SoundScreen extends StatefulWidget {
   final int soundId;
   final String soundName;
   final String path;
-  final String imagePath;
+  final String? imagePath;
   final int themeId;
   
   const SoundScreen({
@@ -14,7 +16,7 @@ class SoundScreen extends StatefulWidget {
     required this.soundId,
     required this.soundName,
     required this.path,
-    required this.imagePath,
+    this.imagePath,
     required this.themeId
   }) : super(key: key);
   
@@ -26,12 +28,97 @@ class SoundScreen extends StatefulWidget {
 class _SoundScreenState extends State<SoundScreen> {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+      backgroundColor: const Color.fromRGBO(63, 68, 75, 1),
       appBar: AppBar(
         title: const Text(MyApp.title),
+        titleTextStyle: const TextStyle(letterSpacing: 2, color: Colors.white,fontSize: 25),
+        backgroundColor: const Color.fromRGBO(51, 55, 61, 1), // status bar color
       ),
       body: Center(
-        child: Text("[${widget.soundId}] ${widget.soundName}"),
+        child:SingleChildScrollView(
+          child: Container(
+
+          margin: const EdgeInsets.all(2.0),
+          padding: const EdgeInsets.all(40.0),
+
+          decoration: BoxDecoration(
+            color: const Color.fromRGBO(63, 68, 75, 1),
+            border: Border.all(
+              color: const Color.fromRGBO(78, 83, 91, 1),
+              width: 6.0,
+            style: BorderStyle.solid),
+            //Border.all
+            borderRadius: const BorderRadius.all(
+            Radius.circular(12),
+            ), //BorderRadius.all
+          ),
+
+          child:Column(
+          children:[
+            const SizedBox(height: 25,),
+            Container(
+              width: 200,
+              height: 200,
+              decoration: widget.imagePath != null ?
+              BoxDecoration(
+                image: DecorationImage(
+                  image: FileImage(
+                    File(widget.imagePath!),
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              )
+              : const BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            const SizedBox(height: 35,),
+            Text("Votre son est [${widget.soundId}] ${widget.soundName}",
+              style: const TextStyle(fontSize: 18, color: Colors.white, letterSpacing: 2),
+            ),
+            const SizedBox(height: 35,),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: <Color>[
+                            Color(0xFF0D47A1),
+                            Color(0xFF1976D2),
+                            Color(0xFF42A5F5),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 250,
+                    child:OutlinedButton.icon(
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.all(16.0),
+                        primary: Colors.white,
+                        textStyle: const TextStyle(fontSize: 18, letterSpacing: 2),
+                      ),
+                      icon: const Icon(
+                        Icons.add,
+                        size: 24.0,
+                      ),
+                      onPressed: () {},
+                      label: const Text('Ajouter un Son'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ]
+            ),
+          ),
+        ),
       ),
     );
   }
