0665fabcf37c8f4469a7a94c033d7ce407504402
toemah <toemah@protonmail.com>
2022-05-26 18:58:07 +0200

    Update soundWidget.dart

--- lib/soundWidget.dart
+++ lib/soundWidget.dart
@@ -52,49 +52,94 @@ class _SoundWidgetState extends State<SoundWidget> {
     }
   }
 
+  Widget actionButton(String text, IconData icon, Function _onPressed) {
+    return MaterialButton(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [Text(text), Icon(icon)],
+      ),
+      onPressed: () => _onPressed,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ButtonTheme(
       minWidth: 100.0,
       height: 100.0,
-      child: widget.imagePath == null
-          ? ElevatedButton(
-              onPressed: play,
-              child: Center(
-                child: Text(
-                  widget.name,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize:
-                          Theme.of(context).textTheme.headline6!.fontSize),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            )
-          : MaterialButton(
-              child: Container(
-                child: Center(
-                  child: Text(
-                    widget.name,
-                    style: TextStyle(
-                        color: Colors.white,
-                        backgroundColor: const Color.fromARGB(127, 0, 0, 0),
-                        fontSize:
-                            Theme.of(context).textTheme.headline6!.fontSize),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: FileImage(
-                      File(widget.imagePath!),
+      child: Stack(
+        children: [
+          widget.imagePath == null
+              ? ElevatedButton(
+                  onPressed: play,
+                  child: Center(
+                    child: Text(
+                      widget.name,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize:
+                              Theme.of(context).textTheme.headline6!.fontSize),
+                      textAlign: TextAlign.center,
                     ),
-                    fit: BoxFit.cover,
                   ),
+                )
+              : MaterialButton(
+                  child: Container(
+                    child: Center(
+                      child: Text(
+                        widget.name,
+                        style: TextStyle(
+                            color: Colors.white,
+                            backgroundColor: const Color.fromARGB(127, 0, 0, 0),
+                            fontSize: Theme.of(context)
+                                .textTheme
+                                .headline6!
+                                .fontSize),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: FileImage(
+                          File(widget.imagePath!),
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  onPressed: play,
+                ),
+          Material(
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: actionButton("Modifier", Icons.edit_outlined, () => {}),
+                    ),
+                    const Divider(),
+                    Expanded(
+                      flex: 1,
+                      child: actionButton("Supprimer", Icons.delete_outlined, () => {}),
+                    ),
+                    const Divider(),
+                    Expanded(
+                      flex: 1,
+                      child: actionButton("Annuler", Icons.cancel_outlined, () => {}),
+                    )
+                  ],
                 ),
               ),
-              onPressed: play,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
             ),
+          ),
+        ],
+      ),
     );
   }
 }
