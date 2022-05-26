8d7d17897f4cb27e27bc034fc357e3e939288484
Florent-W <35995337+Florent-W@users.noreply.github.com>
2022-05-26 20:03:15 +0200

    Page de la liste des sons pour un thème commencée.

--- lib/themescreen.dart
+++ lib/themescreen.dart
@@ -14,6 +14,8 @@ class ThemeScreen extends StatefulWidget {
   State<ThemeScreen> createState() => _ThemeScreenState();
 }
 
+
+
 class _ThemeScreenState extends State<ThemeScreen> {
   @override
   Widget build(BuildContext context) {
@@ -23,7 +25,31 @@ class _ThemeScreenState extends State<ThemeScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Container(),
+        child: Center(
+            child: Column(children : [
+              Row(mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [Padding(padding: EdgeInsets.only(top: 20),
+                      child: Text('THEME 1', style: TextStyle(fontSize: 20)))]),
+              Expanded(child: Scrollbar(child: Container(margin: const EdgeInsets.all(120.0),
+                child: GridView.builder(
+                    gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                        maxCrossAxisExtent: 500,
+                        crossAxisSpacing: 20,
+                        mainAxisSpacing: 20),
+                    itemCount: 100,
+                    itemBuilder: (BuildContext ctx, index) {
+                      return Container(
+                        child:Stack(fit: StackFit.expand,
+                            children: [
+                              Image.asset('assets/images/image.jpeg', fit: BoxFit.cover), Align(alignment: Alignment.center, child: Text((index + 1).toString(), style: const TextStyle(fontSize: 100, color: Colors.purple)))]),
+                        decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            color: Colors.black,
+                            border: Border.all(width: 10)),
+                      );
+                    }),)))])
+          // Text("[${widget.themeId}] ${widget.themeName}"),
+        ),
       )
     );
   }
