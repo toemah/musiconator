7c2929f825ddbf0192102f9e58e7e4edf8676957
toemah <toemah@protonmail.com>
2022-05-26 19:00:19 +0200

    Swaped ThemeScreen and Homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,23 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:musiconator/main.dart';
+import 'package:musiconator/soundtheme.dart';
 
 class Homepage extends StatefulWidget {
-  const Homepage({Key? key}) : super(key: key);
+  final List<SoundTheme> themes;
+
+  const Homepage({
+    Key? key,
+    required this.themes,
+  }) : super(key: key);
+
   @override
   State<Homepage> createState() => _HomepageState();
 }
 
 class _HomepageState extends State<Homepage> {
-
   @override
+    
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(MyApp.title),
       ),
-      body: const Center(
-        child: Text("Hello World"),
+      body: Padding(
+        padding : const EdgeInsets.all(10.0),
+        child: ListView.builder(
+          itemCount: widget.themes.length ,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text( "theme "+ index.toString() + " : " + widget.themes[index].name ),
+              onTap: () => {}, //ajouter la fonction pour aller dans un th�me 
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                icon:const Icon(Icons.edit),
+                onPressed: () { },
+                
+                ), 
+              ]
+              )
+            );
+          },
+        ),
+        
       ),
+      floatingActionButton: FloatingActionButton.extended(  
+        icon : const Icon(Icons.add),
+        label: const Text('Theme'),
+        backgroundColor: Colors.blue,  
+        foregroundColor: Colors.white,  
+        onPressed: () => {
+          widget.themes.add(
+            SoundTheme(id: -1, name: "explosion")//ajouter la modal pour cr�er un th�me
+          )
+        },  
+      ),  
     );
   }
 }
--- lib/themescreen.dart
+++ lib/themescreen.dart
@@ -16,46 +16,15 @@ class ThemeScreen extends StatefulWidget {
 
 class _ThemeScreenState extends State<ThemeScreen> {
   @override
-    
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(MyApp.title),
       ),
       body: Padding(
-        padding : const EdgeInsets.all(10.0),
-        child: ListView.builder(
-          itemCount: widget.themes.length ,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text( "theme "+ index.toString() + " : " + widget.themes[index].name ),
-              onTap: () => {}, //ajouter la fonction pour aller dans un thème 
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                icon:const Icon(Icons.edit),
-                onPressed: () { },
-                
-                ), 
-              ]
-              )
-            );
-          },
-        ),
-        
-      ),
-      floatingActionButton: FloatingActionButton.extended(  
-        icon : const Icon(Icons.add),
-        label: const Text('Theme'),
-        backgroundColor: Colors.blue,  
-        foregroundColor: Colors.white,  
-        onPressed: () => {
-          widget.themes.add(
-            SoundTheme(id: -1, name: "explosion")//ajouter la modal pour créer un thème
-          )
-        },  
-      ),  
+        padding: const EdgeInsets.all(10.0),
+        child: Container(),
+      )
     );
   }
 }
