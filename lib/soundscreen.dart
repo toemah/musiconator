96398521e82088cd6bdc92f0c73f85eefac634fb
toemah <toemah@protonmail.com>
2022-05-28 03:00:32 +0200

    Update soundscreen.dart

--- lib/soundscreen.dart
+++ lib/soundscreen.dart
@@ -5,112 +5,122 @@ import 'package:musiconator/main.dart';
 import 'package:musiconator/sound.dart';
 
 class SoundScreen extends StatefulWidget {
-  final Sound sound;
+  final Sound? sound;
 
-  const SoundScreen({Key? key, required this.sound})
-      : super(key: key);
+  const SoundScreen({Key? key, this.sound}) : super(key: key);
 
   @override
   State<SoundScreen> createState() => _SoundScreenState();
 }
 
 class _SoundScreenState extends State<SoundScreen> {
+  late Sound? sound = widget.sound;
+  TextEditingController soundNameField = TextEditingController();
 
-  late Sound sound = widget.sound;
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      soundNameField.text = sound != null ? sound!.name : "Son ${MyApp.sounds.length}";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(63, 68, 75, 1),
       appBar: AppBar(
         title: const Text(MyApp.title),
-        titleTextStyle: const TextStyle(
-            letterSpacing: 2, color: Colors.white, fontSize: 25),
-        backgroundColor:
-            const Color.fromRGBO(51, 55, 61, 1), // status bar color
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            margin: const EdgeInsets.all(2.0),
-            padding: const EdgeInsets.all(40.0),
-            decoration: BoxDecoration(
-              color: const Color.fromRGBO(63, 68, 75, 1),
-              border: Border.all(
-                  color: const Color.fromRGBO(78, 83, 91, 1),
-                  width: 6.0,
-                  style: BorderStyle.solid),
-              borderRadius: const BorderRadius.all(
-                Radius.circular(12),
-              ),
-            ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: SizedBox(
+            width: 500.0,
             child: Column(
               children: [
-                const SizedBox(
-                  height: 25,
+                TextField(
+                  controller: soundNameField,
+                  style: Theme.of(context).textTheme.headlineMedium,
+                  textAlign: TextAlign.center,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                  ),
                 ),
-                Container(
-                  width: 200,
-                  height: 200,
-                  decoration: sound.imagePath != null
-                      ? BoxDecoration(
-                          image: DecorationImage(
-                            image: FileImage(
-                              File(sound.imagePath!),
+                const Divider(),
+                SizedBox(
+                  height: 500.0,
+                  child: Container(
+                    child: const FittedBox(
+                      child: Icon(
+                        Icons.image_not_supported_outlined,
+                        color: Colors.white,
+                      ),
+                    ),
+                    decoration: sound == null || sound!.imagePath == null
+                        ? BoxDecoration(
+                            color: Theme.of(context).backgroundColor)
+                        : BoxDecoration(
+                            image: DecorationImage(
+                              image: FileImage(
+                                File(sound!.imagePath!),
+                              ),
+                              fit: BoxFit.cover,
                             ),
-                            fit: BoxFit.cover,
                           ),
-                        )
-                      : const BoxDecoration(
-                          color: Colors.blue,
-                        ),
+                  ),
                 ),
-                const SizedBox(
-                  height: 35,
-                ),
-                Text(
-                  "Votre son est [${sound.id}] ${sound.name}",
-                  style: const TextStyle(
-                      fontSize: 18, color: Colors.white, letterSpacing: 2),
-                ),
-                const SizedBox(
-                  height: 35,
-                ),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(4),
-                  child: Stack(
-                    children: <Widget>[
-                      Positioned.fill(
-                        child: Container(
-                          decoration: const BoxDecoration(
-                            gradient: LinearGradient(
-                              colors: <Color>[
-                                Color(0xFF0D47A1),
-                                Color(0xFF1976D2),
-                                Color(0xFF42A5F5),
-                              ],
-                            ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 2.0, right: 2.0),
+                          child: ElevatedButton(
+                            onPressed: () => {},
+                            child: const Text("Supprimer"),
                           ),
                         ),
                       ),
-                      SizedBox(
-                        width: 250,
-                        child: OutlinedButton.icon(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(16.0),
-                            primary: Colors.white,
-                            textStyle:
-                                const TextStyle(fontSize: 18, letterSpacing: 2),
+                      Expanded(
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 2.0, right: 2.0),
+                          child: ElevatedButton(
+                            onPressed: () => {},
+                            child: Text(
+                              sound == null ||
+                                      (sound != null &&
+                                          sound!.imagePath == null)
+                                  ? "Ajouter"
+                                  : "Modifier",
+                            ),
                           ),
-                          icon: const Icon(
-                            Icons.add,
-                            size: 24.0,
-                          ),
-                          onPressed: () {},
-                          label: const Text('Ajouter un Son'),
                         ),
                       ),
                     ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(2.0, 5.0, 2.0, 0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () => {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            sound != null ? sound!.path : "Ajouter un son",
+                          ),
+                          const Icon(
+                            Icons.add_circle_outline,
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
               ],
