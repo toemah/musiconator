081f9537a78ff84be0716b0d75b0bb6f10732ef9
toemah <toemah@protonmail.com>
2022-05-28 15:11:16 +0200

    Update soundscreen.dart

--- lib/soundscreen.dart
+++ lib/soundscreen.dart
@@ -21,7 +21,8 @@ class _SoundScreenState extends State<SoundScreen> {
   void initState() {
     super.initState();
     setState(() {
-      soundNameField.text = sound != null ? sound!.name : "Son ${MyApp.sounds.length}";
+      soundNameField.text =
+          sound != null ? sound!.name : "Son ${MyApp.sounds.length}";
     });
   }
 
@@ -32,98 +33,102 @@ class _SoundScreenState extends State<SoundScreen> {
         title: const Text(MyApp.title),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(MyApp.spacing),
         child: Center(
-          child: SizedBox(
-            width: 500.0,
-            child: Column(
-              children: [
-                TextField(
-                  controller: soundNameField,
-                  style: Theme.of(context).textTheme.headlineMedium,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
-                  ),
-                ),
-                const Divider(),
-                SizedBox(
-                  height: 500.0,
-                  child: Container(
-                    child: const FittedBox(
-                      child: Icon(
-                        Icons.image_not_supported_outlined,
-                        color: Colors.white,
-                      ),
+          child: SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: MyApp.maxWidth),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: soundNameField,
+                    style: Theme.of(context).textTheme.headlineMedium,
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
                     ),
-                    decoration: sound == null || sound!.imagePath == null
-                        ? BoxDecoration(
-                            color: Theme.of(context).backgroundColor)
-                        : BoxDecoration(
-                            image: DecorationImage(
-                              image: FileImage(
-                                File(sound!.imagePath!),
+                  ),
+                  const Divider(),
+                  AspectRatio(
+                    aspectRatio: 1,
+                    child: Container(
+                      child: const FittedBox(
+                        child: Icon(
+                          Icons.image_not_supported_outlined,
+                          color: Colors.white,
+                        ),
+                      ),
+                      decoration: sound == null || sound!.imagePath == null
+                          ? BoxDecoration(
+                              color: Theme.of(context).backgroundColor)
+                          : BoxDecoration(
+                              image: DecorationImage(
+                                image: FileImage(
+                                  File(sound!.imagePath!),
+                                ),
+                                fit: BoxFit.cover,
                               ),
-                              fit: BoxFit.cover,
                             ),
-                          ),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 5.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 2.0, right: 2.0),
-                          child: ElevatedButton(
-                            onPressed: () => {},
-                            child: const Text("Supprimer"),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 2.0, right: 2.0),
-                          child: ElevatedButton(
-                            onPressed: () => {},
-                            child: Text(
-                              sound == null ||
-                                      (sound != null &&
-                                          sound!.imagePath == null)
-                                  ? "Ajouter"
-                                  : "Modifier",
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.only(left: 2.0, right: 2.0),
+                            child: ElevatedButton(
+                              onPressed: () => {},
+                              child: const Text("Supprimer"),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                        Expanded(
+                          flex: 1,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.only(left: 2.0, right: 2.0),
+                            child: ElevatedButton(
+                              onPressed: () => {},
+                              child: Text(
+                                sound == null ||
+                                        (sound != null &&
+                                            sound!.imagePath == null)
+                                    ? "Ajouter"
+                                    : "Modifier",
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(2.0, 5.0, 2.0, 0),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () => {},
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            sound != null ? sound!.path : "Ajouter un son",
-                          ),
-                          const Icon(
-                            Icons.add_circle_outline,
-                          ),
-                        ],
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(2.0, 5.0, 2.0, 0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () => {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              sound != null ? sound!.path : "Ajouter un son",
+                            ),
+                            const Icon(
+                              Icons.add_circle_outline,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
